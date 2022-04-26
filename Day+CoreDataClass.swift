249ebf9ca9ef97fdc40637a46e2645a0c405b7a6
furqan <famanji5010@stu.d214.org>
2022-04-26 14:11:28 -0500

    day entity subclass created

---
+++ Day+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Day+CoreDataClass.swift
+//  WeightLiftApp
+//
+//  Created by furqan amanji on 4/26/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Day)
+public class Day: NSManagedObject {
+
+}
