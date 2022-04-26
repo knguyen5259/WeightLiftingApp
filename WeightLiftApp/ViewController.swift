e3bc49cd258f026784fa1cd46bea6c0397ef3dec
knguyen5259 <91755924+knguyen5259@users.noreply.github.com>
2022-04-26 14:34:27 -0500

    Added tableview delegate

--- WeightLiftApp/ViewController.swift
+++ WeightLiftApp/ViewController.swift
@@ -10,22 +10,20 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    var days: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        let
-        
         tableView.dataSource = self
     }
-        }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
-    UITableView {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return days.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        cell.textLabel?.text = "Monday"
+        cell.textLabel?.text = days[indexPath.row]
         return cell
     }
 
