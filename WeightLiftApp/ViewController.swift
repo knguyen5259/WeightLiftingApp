3610c6c0d6b3df8f32af1cdee5cf9b1c09262069
Frank Tortorella Hreczuch <ftortorellahrecz5374@stu.d214.org>
2022-04-26 14:20:24 -0500

    partially finished with the reusable cell "monday"

--- WeightLiftApp/ViewController.swift
+++ WeightLiftApp/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let
+        
+        tableView.dataSource = self
     }
-
+        }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+    UITableView {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "Monday"
+        return cell
+    }
 
 }
 
