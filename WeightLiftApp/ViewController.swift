8e00a983194a841d828cc212ac3f978a972cf102
Dylan Stukin <dstukin5354@stu.d214.org>
2022-04-26 14:26:06 -0500

    starting to work on prepare method

--- WeightLiftApp/ViewController.swift
+++ WeightLiftApp/ViewController.swift
@@ -14,20 +14,12 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        let
         
         tableView.dataSource = self
     }
-        }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
-    UITableView {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        cell.textLabel?.text = "Monday"
-        return cell
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+
     }
 
 }
-
