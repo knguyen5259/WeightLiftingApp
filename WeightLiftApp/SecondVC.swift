cbeab8b0a37750e4478d9caea4c2de719d237dc3
knguyen5259 <91755924+knguyen5259@users.noreply.github.com>
2022-05-02 14:32:06 -0500

    Added outlets

--- WeightLiftApp/SecondVC.swift
+++ WeightLiftApp/SecondVC.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class SecondVC: UIViewController {
 
+    @IBOutlet weak var workoutOneLabel: UILabel!
+    @IBOutlet weak var workoutTwoLabel: UILabel!
+    @IBOutlet weak var workoutThreeLabel: UILabel!
+    @IBOutlet weak var workoutFourLabel: UILabel!
+    @IBOutlet weak var workoutFiveLabel: UILabel!
+    @IBOutlet weak var workoutSixLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
