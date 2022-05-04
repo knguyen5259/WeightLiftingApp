9421d58f21c466ab569a08a60c84e29ce533a3a9
Dylan Stukin <dstukin5354@stu.d214.org>
2022-05-04 14:28:34 -0500

    added mapView to show gyms in the area. All code is done

---
+++ WeightLiftApp/FourthViewController.swift
@@ -0,0 +1,62 @@
+//
+//  FourthViewController.swift
+//  WeightLiftApp
+//
+//  Created by Dylan Stukin on 5/4/22.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class FourthViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    
+    var Gyms: [MKMapItem] = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+
+    }
+    
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        let myCenter = currentLocation.coordinate
+        let mySpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let myRegion = MKCoordinateRegion(center: myCenter, span: mySpan)
+        mapView.setRegion(myRegion, animated: true)
+        
+    }
+    
+    @IBAction func whenSearchButtonpressed(_ sender: Any) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Gyms"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        
+        search.start { myResponse, myError in
+            guard let response = myResponse else { return }
+            for currentMapItem in response.mapItems {
+                self.Gyms.append(currentMapItem)
+                let annotation = MKPointAnnotation()
+                annotation.title = currentMapItem.name
+                annotation.coordinate = currentMapItem.placemark.coordinate
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+    }
+    
+}
