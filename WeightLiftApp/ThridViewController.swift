dc9a40cce78ce818b7ea275120a8810b3a3c8f9e
Frank Tortorella Hreczuch <ftortorellahrecz5374@stu.d214.org>
2022-05-10 14:31:47 -0500

    changing the list array to the food dictionary

--- WeightLiftApp/ThridViewController.swift
+++ WeightLiftApp/ThridViewController.swift
@@ -12,7 +12,7 @@ class ThridViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet var tableViewTwo: UITableView!
     
-    var list = ["Nut Butter Energy Balls", "Peanut Butter and Banana Smoothie", "Fruit Salad","Vanilla Protien Smoothie", "Cliff Bars", "Pedialyte", "Better Pizza", "Healthy Chicken Quesadillas", "Costco Rotisserie Chicken"]
+    var food = ["Nut Butter Energy Balls": "https://chefsavvy.com/5-ingredient-peanut-butter-energy-bites/", "Peanut Butter and Banana Smoothie": "https://www.wellplated.com/peanut-butter-banana-smoothie/", "Fruit Salad": "https://www.shelovesbiscotti.com/healthy-fruit-salad-recipe/","Vanilla Protien Smoothie": "https://www.amydgorin.com/vanilla-bean-smoothie-high-protein-vanilla-smoothie/", "Cliff Bars": "https://shop.clifbar.com/products/clif-bar-chocolate-chip", "Pedialyte": "https://pedialyte.com/?gclsrc=aw.ds&gclid=EAIaIQobChMIqJ2_p9TV9wIVknxvBB32sAkkEAAYASAAEgIN9fD_BwE", "Better Pizza": "https://www.acouplecooks.com/best-healthy-pizza-recipe/", "Healthy Chicken Quesadillas": "https://www.slenderkitchen.com/recipe/healthy-chicken-quesadillas"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,20 +20,18 @@ class ThridViewController: UIViewController, UITableViewDataSource {
         tableViewTwo.dataSource = self
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return food.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableViewTwo.dequeueReusableCell(withIdentifier: "myCellTwo", for: indexPath)
-        cell.textLabel?.text = list[indexPath.row]
+        cell.textLabel?.text = Array(food.keys)[indexPath.row]
         return cell
     }
  
-    @IBAction func buttonPressed(_ sender: Any) {
-        if let urk = URL(string: "https://www.blendtec.com/blogs/news/5-energy-pre-workout-smoothie-recipes") {
-            let sorryVC = SFSafariViewController(url: urk)
-            
-            present(sorryVC, animated: true, completion: nil)
-        }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        
     }
     
 }
