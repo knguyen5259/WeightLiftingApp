d9c919949d55ab08b8f52ab1fb47b2c4c1ad179d
furqan <famanji5010@stu.d214.org>
2022-05-16 14:47:57 -0500

    added view controller and table view

---
+++ WeightLiftApp/sixthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  sixthViewController.swift
+//  WeightLiftApp
+//
+//  Created by furqan amanji on 5/16/22.
+//
+
+import UIKit
+
+class sixthViewController: UIViewController, UITableViewDataSource{
+
+
+    @IBOutlet weak var TableView: UITableView!
+    
+    var videoworkouts = ["curls", "bench", "squat", "deadlift"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        TableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videoworkouts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell6 = TableView.dequeueReusableCell(withIdentifier: "Cell6", for: indexPath)
+        cell6.textLabel?.text = videoworkouts[indexPath.row]
+        return cell6
+        
+  
+    }
+    
+}
